87c002f01c21362d4e3c89fbf456527f496f5501
Mohd Jalaluddin Ahmad <9w2tja@gmail.com>
2019-03-22 19:14:42 +0800

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,22 +1,22 @@
-/* The students’ corner store in a university needs a program to implement its the 
+/* The studentsâ€™ corner store in a university needs a program to implement its the 
 student discount policy. The program  is to prompt the user to enter the 
 purchase total and to indicate whether the purchaser is a student. Student are
 eligible to receive a 20% on all the purchases. The discount 
 calculation, however, must be done before addition of the 5% sales tax. This is 
 done to ensure that the discount is not applied to the sales tax. Here are the 
-simple output file – one for a student purchaser and one for a nonstudent purchaser. 
+simple output file â€“ one for a student purchaser and one for a nonstudent purchaser. 
 
 student purchaser: 
-Total purchases				$ 122.00
-Student’s discount (20%)	24.40
-Discounted total			97.60
-Sales tax (5%)				4.88
-Total						$ 102.48
+Total purchases			$ 122.00
+Studentâ€™s discount (20%)	24.40
+Discounted total		97.60
+Sales tax (5%)			4.88
+Total				$ 102.48
 
 nonstudent: 
-Total Purchases				$ 24.90
-Sales tax (5%)				1.25
-Total						$ 26.15
+Total Purchases			$ 24.90
+Sales tax (5%)			1.25
+Total				$ 26.15
 
 Note: to display a % sign, place two % signs in the format string; 
 Printf("%d%%", SALES_TAX);
@@ -32,19 +32,19 @@ int main(void){
 
 	printf("Student <y/n> : ");
 	scanf("%c", &student);
-	printf("Enter purchase total : RM");
+	printf("Enter purchase total : $");
 	scanf("%lf", &purchase);
 	
-	printf("Total purchases \t\t RM%.2lf\n", purchase);
+	printf("Total purchases $%.2lf\n", purchase);
 	if (student=='y'){
 		discount = purchase*(STD_DISCOUNT/100);
-		printf("Student’s discount (%.0lf%%) \t RM%.2lf\n", STD_DISCOUNT, discount);
+		printf("Studentâ€™s discount (%.0lf%%) %.2lf\n", STD_DISCOUNT, discount);
 		purchase = purchase-discount;
-		printf("Discounted total \t\t RM%.2lf\n", purchase);
+		printf("Discounted total %.2lf\n", purchase);
 	}
 	sales_tax = purchase*(TAX/100);
-	printf("Sales tax (%.0lf%%) \t\t\t RM%.2lf\n", TAX, sales_tax);
-	printf("Total \t\t\t\t RM%.2lf\n", purchase+sales_tax);
+	printf("Sales tax (%.0lf%%) %.2lf\n", TAX, sales_tax);
+	printf("Total $%.2lf\n", purchase+sales_tax);
 	
 	return 0;
 }
