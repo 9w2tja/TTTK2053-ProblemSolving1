94a7444200e24d2426c946ba24f5f0bc92093537
Mohd Jalaluddin Ahmad <9w2tja@gmail.com>
2019-03-22 19:23:39 +0800

    Update 7b.c

--- 7b.c
+++ 7b.c
@@ -1,11 +1,8 @@
 /* Modify your program to display the average speed of a vehicle (a real number)at the end of the run */
 
 #include <stdio.h>
-int speed, slow=0, medium=0, high=0, total_speed, avg_speed;
+int speed=70, slow=0, medium=0, high=0, total_speed, avg_speed;
 int main (void){
-
-	printf("Enter speed of vehicles (in miles) : ");
-	scanf("%i", &speed);
 	
 	if (speed != 0){
 		if (speed < 50)
@@ -15,7 +12,6 @@ int main (void){
 		else
 			high++;
 		total_speed += speed;
-		return main();
 	}
 	
 	avg_speed = total_speed/(high+medium+slow);
